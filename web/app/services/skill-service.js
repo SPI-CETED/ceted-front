66dc20d0170f0ad9c31a7006aeddbacc6ca68051
CassianoNeves <cassianodasnevesc10@gmail.com>
2016-12-02 21:05:49 -0200

    feat(integration-skill)

---
+++ web/app/services/skill-service.js
@@ -0,0 +1,33 @@
+(function() {
+	'use strict';
+
+	function ProfileService($http, CONFIG) {
+    var self = this;
+    var BASE_URL = 'http://localhost:8084/v1/habilities';
+
+    self.create = function(hability) {
+      return $http.post(BASE_URL, hability);
+		};
+
+		self.list = function() {
+      return $http.get(BASE_URL + '/list');
+		};
+
+		self.delete = function(id) {
+      return $http.delete(BASE_URL + '/' + id);
+		};
+
+		self.find = function(id) {
+      return $http.get(BASE_URL + '/' + id);
+		};
+	}
+
+	module.exports = angular.module('skillService', [])
+		.service('skillService', [
+      '$http',
+      'CONFIG',
+			ProfileService
+		])
+		.name;
+
+})();
